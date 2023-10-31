a6720392ed5a91448fe783f081ad746b96f25abf
tenyapious <tenyapious@gmail.com>
2023-10-31 18:39:14 +0000

    print negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 1:
-    last_digit = (number * -1) % 10
+if number < 0:
+    last_digit = number % -10
 else:
     last_digit = number % 10
 text = 'Last digit of {} is {} and is {}'
@@ -10,5 +10,5 @@ if last_digit > 5:
     print(text.format(number, last_digit, 'greater than 5'))
 elif last_digit == 0:
     print(text.format(number, last_digit, '0'))
-elif last_digit < 6 and last_digit > 0:
+elif last_digit < 6 and not 0:
     print(text.format(number, last_digit, 'less than 6 and not 0'))
