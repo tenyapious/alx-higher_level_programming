a519d64365d7b4db80c4dc8a9aa10a04fbd30afe
tenyapious <tenyapious@gmail.com>
2023-11-02 15:21:01 +0000

    replace return with exit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,27 +3,25 @@ if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
 
-    def my_calculator():
-        argv_len = len(argv) - 1
-        if argv_len != 3:
-            print("Usage: {} <a> <operator> <b>".format(argv[0]))
-            return (1)
+    argv_len = len(argv) - 1
+    return_val = 0
+    if argv_len != 3:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
 
-        a = int(argv[1])
-        b = int(argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
 
-        match argv[2]:
-            case "+":
-                print("{} + {} = {}".format(a, b, add(a, b)))
-            case "-":
-                print("{} - {} = {}".format(a, b, sub(a, b)))
-            case "*":
-                print("{} * {} = {}".format(a, b, mul(a, b)))
-            case "/":
-                print("{} / {} = {}".format(a, b, div(a, b)))
-            case _:
-                print("Unknown operator. Available operators: +, -, * and /")
-                return (1)
-        return (0)
-
-    my_calculator()
+    match argv[2]:
+        case "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        case "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        case "*":
+             print("{} * {} = {}".format(a, b, mul(a, b)))
+        case "/":
+            print("{} / {} = {}".format(a, b, div(a, b)))
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    exit(0)
