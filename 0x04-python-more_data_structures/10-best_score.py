46ee30b15ea8b48bd96bc44420bf9c5130125bba
tenyapious <tenyapious@gmail.com>
2023-11-08 14:26:16 +0000

    Check if dictionary is not empty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary):
         best_score_val = 0
         for key in list(a_dictionary):
             if a_dictionary[key] > best_score_val:
