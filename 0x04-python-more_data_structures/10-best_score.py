f55c1f47a34000a1cf747fc6c6db86aad7b2b76d
tenyapious <tenyapious@gmail.com>
2023-11-08 14:35:15 +0000

    return dictionary key not value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None and len(a_dictionary):
+        best_score_key = ''
         best_score_val = 0
         for key in list(a_dictionary):
             if a_dictionary[key] > best_score_val:
+                best_score_key = key
                 best_score_val = a_dictionary[key]
-        return best_score_val
+        return best_score_key
     return None
