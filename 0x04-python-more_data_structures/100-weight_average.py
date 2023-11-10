41097882106ccca84284cd2f6369d3f0a16b8338
tenyapious <tenyapious@gmail.com>
2023-11-10 11:12:34 +0000

    return 0 if list is empty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    total = 0
-    qty = 0
-    for t in my_list:
-        i, j = t
-        total += i * j
-        qty += j
-    return total/qty
+    if len(my_list):
+        total = 0
+        qty = 0
+        for t in my_list:
+            i, j = t
+            total += i * j
+            qty += j
+        return total/qty
+    return 0
