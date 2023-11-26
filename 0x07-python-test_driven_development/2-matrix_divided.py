a8fb5b1dac46820d63b8db6df3e3f9fc77e82713
tenyapious <tenyapious@gmail.com>
2023-11-26 21:49:44 +0000

    Document function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
 
-def matrix_divided(matrix, div):
+""" Defines a function matrix_divided """
 
+def matrix_divided(matrix, div):
+    """ divide every element of a matrix by a number and returns a new matrix
+        containing the results of the divisions 
+
+    Args:
+        matrix (list): This is a list of lists and containing integers/floats
+            that are divided by `div`
+        div (int/float): divides the elements of `matrix`
+
+    Returns:
+        list: a new list if successful
+
+    Raises:
+        TypeError: If `matrix` is not a list of lists
+            If outer list of `matrix` is empty
+            If an inner list of `matrix` is empty
+            If inner lists of `matrix` are not the same sizes
+            If an element of a list is not an integer or float
+            If `div` is not an integer or float
+
+        ZeroDivisionError: If `div` is 0
+    """
+ 
     if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
