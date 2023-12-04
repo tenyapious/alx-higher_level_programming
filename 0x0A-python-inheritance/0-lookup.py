ecd068ed2e1f57b39b3dab8eef593d1c594747c6
tenyapious <tenyapious@gmail.com>
2023-12-04 20:08:21 +0000

    Document the function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
+""" Defines a function lookup """
+
+
 def lookup(obj):
+    """ lookups the attributes and methods of available to objects
+
+    Args:
+        obj: the object to lookup
+
+    Return:
+        list: Available attributes and methods
+
+    """
+
     return dir(obj)
