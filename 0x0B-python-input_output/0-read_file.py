64a4cac3292c50d41d5c446095a548647de06db5
tenyapious <tenyapious@gmail.com>
2023-12-05 12:45:34 +0000

    Write a function to read a file and prints to the stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Defines read_file function """
+
+
+def read_file(filename=""):
+    """
+    Read file and prints it to the stdout.
+
+    Args:
+        filename: The file to read from.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
