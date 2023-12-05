1b89606ca1df76ed749b4db79abe7d277488d877
tenyapious <tenyapious@gmail.com>
2023-12-05 16:29:08 +0000

    Ensure script is only directly

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """ Define add_item function """
 import sys
-import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    save_to_json_file([], "add_item.json")
-    my_list = []
+    args = sys.argv
 
-for idx, arg in enumerate(args):
-    if idx > 0:
-        my_list.append(arg)
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        save_to_json_file([], "add_item.json")
+        my_list = []
 
-save_to_json_file(my_list, "add_item.json")
+    for idx, arg in enumerate(args):
+        if idx > 0:
+            my_list.append(arg)
+
+    save_to_json_file(my_list, "add_item.json")
