1e7578f14248cff1077e98cc6493750dc77586dc
tenyapious <tenyapious@gmail.com>
2023-12-11 21:17:18 +0000

    Ran 1 test for the class Rectangle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Define a test class for Rectangle """
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ Test class for Rectangle """
+
+    def test_rectangle(self):
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 2)
