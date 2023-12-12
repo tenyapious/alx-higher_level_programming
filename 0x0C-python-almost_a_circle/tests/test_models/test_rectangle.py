ef4246c9bd3659cb639785f3410acd8a16c464a2
tenyapious <tenyapious@gmail.com>
2023-12-12 11:57:51 +0000

    minor changes

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,6 +4,7 @@
 import unittest
 from unittest.mock import patch
 from models.rectangle import Rectangle
+from models.square import Square
 from io import StringIO
 
 
@@ -69,3 +70,7 @@ class TestRectangle(unittest.TestCase):
 
         r1.update(x=1, height=2, y=3, width=4)
         self.assertEqual(str(r1), '[Rectangle] (8) 1/3 - 4/2')
+
+    def test_task10(self):
+        s1 = Square(5)
+        self.assertEqual(str(s1), '[Square] (7) 0/0 - 5')
