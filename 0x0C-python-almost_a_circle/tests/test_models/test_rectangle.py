64e26df7d4d2ec982067d48dde715f7ed0004992
tenyapious <tenyapious@gmail.com>
2023-12-12 01:06:49 +0000

    Test for the method display

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,7 +2,9 @@
 
 """ Define a test class for Rectangle """
 import unittest
+from unittest.mock import patch
 from models.rectangle import Rectangle
+from io import StringIO
 
 
 class TestRectangle(unittest.TestCase):
@@ -17,3 +19,11 @@ class TestRectangle(unittest.TestCase):
 
         r3 = Rectangle(3, 2)
         self.assertEqual(r3.area(), 6)
+
+        displayed_output = StringIO()
+        with patch('sys.stdout', new = displayed_output):
+            r4 = Rectangle(1, 1)
+            r4.display()
+
+            printed_str = displayed_output.getvalue().strip()
+            self.assertEqual(printed_str, '#')
