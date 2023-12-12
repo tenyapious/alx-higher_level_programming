c25d21deff1994ba88f4637c4242801a6c515438
tenyapious <tenyapious@gmail.com>
2023-12-12 02:32:41 +0000

    Add a public method 'update' to update the rectangle values

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,15 @@ class Rectangle(Base):
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} \
 - {self.width}/{self.height}"
+
+    def update(self, *args):
+        if len(args) >= 5:
+            self.y = args[4]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 1:
+            self.id = args[0]
