b0fb1bfc9b979edc873a0411107d2678b6d178b7
tenyapious <tenyapious@gmail.com>
2023-12-13 12:37:03 +0000

    Run test for the public getter setter size in class Square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -14,3 +14,8 @@ class TestSquare(unittest.TestCase):
     def test_task10(self):
         s1 = Square(5)
         self.assertEqual(str(s1), '[Square] (8) 0/0 - 5')
+
+    def test_task11(self):
+        s1 = Square(5)
+        s1.size = 10
+        self.assertEqual(str(s1), '[Square] (9) 0/0 - 10')
