52e22a5c1a259ffe231621dab4d2bf327c9b2dc1
tenyapious <tenyapious@gmail.com>
2023-12-13 13:01:33 +0000

    Write test for task 12

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -19,3 +19,22 @@ class TestSquare(unittest.TestCase):
         s1 = Square(5)
         s1.size = 10
         self.assertEqual(str(s1), '[Square] (9) 0/0 - 10')
+
+    def test_task12(self):
+        s1 = Square(5)
+        self.assertEqual(str(s1), '[Square] (10) 0/0 - 5')
+        
+        s1.update(20)
+        self.assertEqual(str(s1), '[Square] (20) 0/0 - 5')
+
+        s1.update(1, 2)
+        self.assertEqual(str(s1), '[Square] (1) 0/0 - 2')
+
+        s1.update(1, 2, 3)
+        self.assertEqual(str(s1), '[Square] (1) 3/0 - 2')
+
+        s1.update(1, 2, 3, 4)
+        self.assertEqual(str(s1), '[Square] (1) 3/4 - 2')
+
+        s1.update(x=12)
+        self.assertEqual(str(s1), '[Square] (1) 12/4 - 2')
