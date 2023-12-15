7c33b8c0f831923b82595e3f80d8340b08f67759
tenyapious <tenyapious@gmail.com>
2023-12-15 14:49:04 +0000

    Update the class Base by adding a static method that returns the list of the JSONstring representation of a json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,3 +47,15 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns the list of the JSON string representation json_string
+
+        Args:
+            json_string: the json string
+        """
+        if json_string is None or len(json_string) <= 0:
+            return "[]"
+
+        return json.loads(json_string)
