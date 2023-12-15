7df419fe6105c513bb49824124d1cb957bd0d45b
tenyapious <tenyapious@gmail.com>
2023-12-15 14:43:06 +0000

    Update the class Base by adding a class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,8 +24,26 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Returns the JSON string representation of list_dictionaries """
+        """ Returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries: list of dictionaries
+        """
         if list_dictionaries is None or len(list_dictionaries) <= 0:
             return "[]"
 
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list objects to a file
+
+        Args:
+            list_objs: list objects
+        """
+        with open(cls.__name__ + ".json", 'w', encoding="utf-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                f.write(Base.to_json_string(list_dicts))
